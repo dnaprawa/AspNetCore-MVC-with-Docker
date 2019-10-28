67cd6ba5ae9c90d96e259e55d8edd495e93666ac
Damian N <dnaprawa@gmail.com>
2019-10-28 13:08:23 +0100

    Init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LinuxAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
